eeffa5cfe917f2752284e9d13b755c4435bc40fd
Kelly Hansen <kellyhansen182@gmail.com>
2020-11-10 09:54:16 -0800

    Refactored render function to return element, API load will append

--- js/main.js
+++ js/main.js
@@ -1,50 +1,55 @@
+var $main = document.querySelector('main');
+
 var launchList;
 var xhrLaunches = new XMLHttpRequest();
 xhrLaunches.open('GET', 'https://ll.thespacedevs.com/2.0.0/launch/upcoming/?format=json');
 xhrLaunches.responseType = 'json';
 xhrLaunches.addEventListener('load', function () {
   launchList = xhrLaunches.response;
-  renderUpcomingLaunches();
-});
-xhrLaunches.send();
-
-function renderUpcomingLaunches() {
   if (!launchList.results) {
     const $loadingMsg = document.querySelector('.loading-msg');
     $loadingMsg.textContent = launchList.detail;
-    return;
+  } else {
+    $main.appendChild(renderUpcomingLaunches(launchList));
   }
-  const $main = document.querySelector('main');
+});
+xhrLaunches.send();
+
+function renderLaunchItem(launchAPIData, i) {
+  const $launchItem = document.createElement('button');
+  $launchItem.className = 'launch-item';
+  $launchItem.setAttribute('data-id', i);
+
+  const $launchItemImg = document.createElement('img');
+  $launchItemImg.src = 'images/rocketwhite.png';
+  $launchItemImg.alt = 'Rocket icon';
+  $launchItem.appendChild($launchItemImg);
+
+  const $launchItemP = document.createElement('p');
+  $launchItemP.textContent = launchAPIData.results[i].name;
+  $launchItem.appendChild($launchItemP);
+
+  return $launchItem;
+}
+
+function renderUpcomingLaunches(launchAPIData) {
   const $existingSection = document.querySelector('section');
   $main.removeChild($existingSection);
 
   const $newSection = document.createElement('section');
-  $main.appendChild($newSection);
 
   const $h1 = document.createElement('h1');
   $h1.textContent = 'Upcoming Rocket Launches';
   $newSection.appendChild($h1);
 
   for (let i = 0; i < 10; i++) {
-    const $launchItem = document.createElement('button');
-    $launchItem.className = 'launch-item';
-    $launchItem.setAttribute('data-id', i);
-
-    const $launchItemImg = document.createElement('img');
-    $launchItemImg.src = 'images/rocketwhite.png';
-    $launchItemImg.alt = 'Rocket icon';
-    $launchItem.appendChild($launchItemImg);
-
-    const $launchItemP = document.createElement('p');
-    $launchItemP.textContent = launchList.results[i].name;
-    $launchItem.appendChild($launchItemP);
-
-    $newSection.appendChild($launchItem);
+    $newSection.appendChild(renderLaunchItem(launchAPIData, i));
   }
 
   const $grayButton = document.createElement('button');
   $grayButton.className = 'gray-button';
-  $grayButton.textContent = 'View Past Launches';
+  $grayButton.textContent = 'View Recent Launches';
   $newSection.appendChild($grayButton);
 
+  return $newSection;
 }
