4cf3c1cc15d8f37bdd814b484298cabb133a1965
Kelly Hansen <kellyhansen182@gmail.com>
2020-11-10 11:34:33 -0800

    Declare previousLaunchList;

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ var currentView = 'upcoming';
 var $main = document.querySelector('main');
 
 var upcomingLaunchList;
+var previousLaunchList;
 var xhrLaunches = new XMLHttpRequest();
 xhrLaunches.open('GET', 'https://ll.thespacedevs.com/2.0.0/launch/upcoming/?format=json');
 xhrLaunches.responseType = 'json';
